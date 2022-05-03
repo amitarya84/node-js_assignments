f07c8397688394112164e663b1ccb5ff8f154be0
= <amitaryawrk@gmail.com>
2022-05-03 20:13:15 +0530

    node assignment

---
+++ Assignment_7/Backend/middleware/is-auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1];
+  console.log(token)
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, 'someSecret');
+  } catch (err) {
+    console.log('here is the err')
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
